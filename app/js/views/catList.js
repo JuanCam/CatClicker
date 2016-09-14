aa73b591c9ba9d18e138f5254c13534a317c4f84
jgutierrez <juancgr_4@hotmail.com>
2016-09-14 11:23:28 -0500

    New template features (foreach loop)

--- app/js/views/catList.js
+++ app/js/views/catList.js
@@ -8,8 +8,6 @@
     w.catListView.init = function() {
         var view = this;
         this.tmplPath = w.octopus.tmplPath;
-        this.ul = document.createElement('ul');
-        this.el.appendChild(this.ul);
         this.getTmpl().then(function(text) {
 
             view.refresh(text);
@@ -19,19 +17,11 @@
     w.catListView.render = function() {
         var c = 0;
         var cats = w.octopus.getCats();
-        this.ul.innerHTML = '';
-
-        function buildMarkup() {
-            if (c < cats.length) {
-                this.ul.innerHTML += this.build(cats[c]);
-                this.ul.addEventListener('click', clickCatItem);
-                c++;
-                return buildMarkup.call(this);
-            } else {
-                return true;
-            }
-        }
-        buildMarkup.call(this);
+        this.el.innerHTML = this.build({
+            'Cats': cats,
+            'TitleList': 'Cat List'
+        });
+        this.el.addEventListener('click', clickCatItem);
     }
 
     function clickCatItem(event) {
--- app/js/views/core/viewDef.js
+++ app/js/views/core/viewDef.js
@@ -5,6 +5,7 @@
         return new View(attributes);
     };
 
+
     function View(attributes) {
         return {
             el: attributes.el,
@@ -24,12 +25,25 @@
             build: function(data) {
                 /*Build the html based on the data object*/
                 var html = this.html;
+                var expectField = new RegExp('[{]{2}[@/a-zA-Z0-9]+(?=[}]{2})[}]{2}', 'g');
+
                 for (var field in data) {
-                    var regExp = new RegExp('[{]{2}[' + field + ']+(?=[}]{2})[}]{2}', 'g');
-                    if (regExp.test(html)) {
-                        html = html.replace(regExp, (data[field] === undefined) ? '' : data[field]);
+
+                    var structs = findField(field, html);
+                    if (isLoop(structs, field)) {
+                        if (!(data[field] instanceof Array)) {
+                            console.error('data must be an array when makred @ (loop)');
+                            return;
+                        }
+                        var scopes = findScopes(structs, html);
+                        var htmlList = replaceScopes(scopes, data[field]);
+                        html = putHtml.call(html, htmlList, scopes);
+                        html = cleanHTML.call(html, structs);
+                    } else {
+                        html = printVariables.call(html, field, data[field]);
                     }
                 }
+
                 return html;
             },
             refresh: function(html) {
@@ -37,6 +51,135 @@
                 this.html = html;
             }
         }
+
+        /*Private methods*/
+
+
+        function findField(field, html) {
+
+            var reg = new RegExp('[{]{2}[@/' + field + ']+(?=[}]{2})[}]{2}', 'g')
+            return html.match(reg);
+        }
+
+
+        function printVariables(field, data) {
+
+            var reg = new RegExp('[{]{2}[' + field + ']+(?=[}]{2})[}]{2}', 'g');
+            return this.replace(reg, (data === undefined) ? '' : data);
+        }
+
+        function findExpr(str) {
+
+            return this.search(new RegExp(str, 'g'));
+        }
+
+        function cleanHTML(structs) {
+            var s = 0;
+            var html = this;
+
+            return clean(structs);
+
+            function clean(structs) {
+                if (s < structs.length) {
+                    html = html.replace(new RegExp(structs[s], 'g'), '');
+                    s++;
+                    return clean(structs);
+                } else {
+                    return html;
+                }
+            }
+        }
+        /*Each loop - template functionality*/
+
+        function isLoop(structs, field) {
+
+            return (structs) ? structs.join('').search(new RegExp('@' + field, 'g')) > -1 : structs;
+        }
+
+        function findScopes(structs, html) {
+            var s = 0;
+            var scopes = [];
+            var htmlScope = '';
+
+            return findScope(structs, html);
+
+            function findScope(structs, html) {
+                if (s < structs.length) {
+                    if (s % 2 == 0) {
+                        var index = findExpr.call(html, structs[s]) + structs[s].length;
+                        htmlScope = html.slice(index);
+                    } else {
+                        var index = findExpr.call(htmlScope, structs[s]);
+                        htmlScope = htmlScope.slice(0, index);
+                        scopes.push(htmlScope);
+                        html = html.replace(structs[s - 1] + htmlScope + structs[s], '');
+                    }
+                    s++;
+                    return findScope(structs, html);
+                } else {
+                    return scopes;
+                }
+            }
+        }
+
+        function putHtml(htmlList, scopes) {
+            var s = 0;
+            var html = this;
+
+            return put(scopes);
+
+            function put(scopes) {
+                if (s < scopes.length) {
+                    html = html.replace(scopes[s], htmlList[s]);
+                    s++;
+                    return put(scopes);
+                } else {
+                    return html;
+                }
+            }
+        }
+
+        function replaceScopes(scopes, data) {
+            var s = 0;
+            var htmlList = [];
+
+            function replaceScope(scopes, data) {
+                var scope = scopes[s]
+                if (s < scopes.length) {
+                    htmlList.push(printEach(scope, data));
+                    s++;
+                    return replaceScope(scopes, data);
+                } else {
+                    return htmlList;
+                }
+            }
+            return replaceScope(scopes, data);
+        }
+
+        function printEach(scope, list) {
+
+            var d = 0;
+            var htmlList = '';
+
+            return each(list, scope);
+
+            function each(list, scope) {
+                var eachScope = scope;
+                if (d < list.length) {
+                    for (var field in list[d]) {
+                        var data = list[d][field];
+                        eachScope = printVariables.call(eachScope, field, data);
+                    }
+                    htmlList += eachScope;
+                    d++;
+                    return each(list, scope);
+                } else {
+                    return htmlList;
+                }
+            }
+
+        }
+
     }
 
 
